a5a1205e8cf99932d0ffe1834023d9bbfad623ce
rain athkravisoonthorn <107684386+areain10@users.noreply.github.com>
2024-04-26 11:01:18 -0700

    added starting room/updated map, edits to convo scene

--- Assets/ConversationUI.cs
+++ Assets/ConversationUI.cs
@@ -20,9 +20,12 @@ public class ConversationUI : MonoBehaviour
     private bool isPlayerTalking;
     private bool[] convoDone;
     public bool inconvo;
+    private Vector3 ogSize = new Vector3(1,1,1);
+    private Vector3 idleSize = new Vector3(1,1,1) * 0.85f;
 
-    //uitext 
+    //uielements
     public TextMeshProUGUI speech;
+    public Image playerSprite;
     private void Awake()
     {
         instance = this;
@@ -87,7 +90,7 @@ public class ConversationUI : MonoBehaviour
         switch (isPlayerTalking)
         {
             //if player is talking, show player dialogue
-            case true:speech.text = convoS.PlayerDialogue[convoStep[0]];
+            case true:playerSprite.color = Color.white; playerSprite.gameObject.transform.localScale = ogSize; npcSprite.gameObject.transform.localScale = idleSize; npcSprite.color = Color.gray; speech.text = convoS.PlayerDialogue[convoStep[0]];
                 if ((convoStep[0] + 1) == convoS.PlayerDialogue.Length)
                 {
                     //if player dialogue ended then change done
@@ -98,7 +101,8 @@ public class ConversationUI : MonoBehaviour
 
                 break;
             //same thing but with npc dialogue
-            case false:speech.text = convoS.NPCDialogue[convoStep[1]];
+            case false:
+                npcSprite.color = Color.white; npcSprite.gameObject.transform.localScale = ogSize; playerSprite.gameObject.transform.localScale = idleSize; playerSprite.color = Color.gray; speech.text = convoS.NPCDialogue[convoStep[1]];
                 if ((convoStep[1] + 1) == convoS.NPCDialogue.Length)
                 {
                     convoDone[1] = true;
